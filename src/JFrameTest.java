b9a26e32657a6172b5bf524e642f487e3c777388
pascal.schellenberg <pascalschellenberg@imac17.local>
2014-08-28 13:03:00 +0200

    Text und Knopf in Fenster

--- src/JFrameTest.java
+++ src/JFrameTest.java
@@ -1,14 +1,27 @@
-import javax.swing.JFrame;
+import javax.swing.*;
 
 //Fenster, das absolut nichts kann
 public class JFrameTest extends JFrame {
-
+	JLabel text;
+	JButton button;
 	public JFrameTest(){
+		//Window's Properties
 		setVisible(true);
 		setSize(600,400);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);//alternativ setLocation(X,Y);
 		setResizable(false);
+		
+		//Text's Properties
+		text = new JLabel ("Hello Warpgate");
+		text.setLocation(10, 10);
+		add(text);
+		
+		//Button's Properties
+		button = new JButton ("Try It!");
+		button.setBounds(10, 10, 100, 25);
+		add(button);
+		
 	}
 	
 	public static void main(String[] args){
