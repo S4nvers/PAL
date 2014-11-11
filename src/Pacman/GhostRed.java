f0f5081e7e2adcbbc49a9404d99a6cd47de8d322
Helibum1 <lukas.boner@stud.ksimlee.ch>
2014-11-11 10:07:17 +0100

    Started to implement Movement-Modes.(LB:No idea what I'm doing)LB, AF

---
+++ src/Pacman/GhostRed.java
@@ -0,0 +1,24 @@
+package Pacman;
+
+public class GhostRed {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		do{
+			if(Timer.Modi == 0){
+				
+			}
+			else if(Timer.Modi == 1){
+				
+			}
+			else if(Timer.Modi == 2){
+				
+			}
+		}while(Timer.GameRunning == true);
+
+	}
+
+}
---
+++ src/Pacman/MovementFear.java
@@ -0,0 +1,13 @@
+package Pacman;
+
+public class MovementFear extends Movement {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
