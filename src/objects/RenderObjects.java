311a1c14c4ee1c7618cc7308d7e7a0cdf57252fe
Helibum1 <lukas.boner@stud.ksimlee.ch>
2014-12-02 10:08:09 +0100

    Continued with last commit. LB

--- src/objects/RenderObjects.java
+++ src/objects/RenderObjects.java
@@ -1,5 +1,42 @@
 package objects;
 
-public class RenderObjects {
+import java.util.HashSet;
+import java.util.Set;
+
+import Pacman.Canvas;
+
+public abstract class RenderObjects implements Comparable<RenderObjects>{
+	
+	protected boolean Collision = true;
+	
+	protected int x;
+	
+	protected int y;
+	
+	public static boolean BoundingBoxes = true;
+	
+	
+	
+	public RenderObjects(int x, int y, boolean Collision) {
+		this.x = x;
+		this.y = y;
+		this.Collision = Collision;
+	}
+	
+	public static void update(Game game) {
+		
+	}
+	
+	public RenderObjects move(int dx, int dy, Set<RenderObjects> allObjects){
+		Set<RenderObjects> collisionTargets = new HashSet<RenderObjects>();
+		if(Collision) {
+			for(RenderObjects object : allObjects) {
+				if(object != this && object.Collision) {
+					collisionTargets.add(object);
+				}
+			}
+			
+		}
+	}
 
 }
