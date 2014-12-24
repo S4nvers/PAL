c1a533c5bf1a119ff77554f05223d5dbadcba8b6
Helibum1 <lukas.boner@stud.ksimlee.ch>
2014-12-24 13:44:56 +0100

    started implementing controls of the player.LB

--- src/Pacman/Pacman.java
+++ src/Pacman/Pacman.java
@@ -1,5 +1,44 @@
 package pacman;
 
-public class Pacman {
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
+public class Pacman implements KeyListener {
+	
+	Pacman.addKeyListener(new KeyListener() {
+		public void keyPressed(java.awt.event.ActionEvent e) {
+	}
+	
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if (e.getKeyCode() == KeyEvent.VK_LEFT){
+			
+		}
+		
+		if (e.getKeyCode() == KeyEvent.VK_RIGHT){
+			
+		}
+		
+		if (e.getKeyCode() == KeyEvent.VK_UP){
+			
+		}
+		
+		if (e.getKeyCode() == KeyEvent.VK_DOWN){
+			
+		}
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+});
 }
